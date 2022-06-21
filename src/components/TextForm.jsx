c7f26aa21c81fc8b0679727f30b7325cdfc0653b
sanjay jangra <84704800+sanjay632@users.noreply.github.com>
2022-06-21 15:54:45 +0530

    Update TextForm.jsx

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -28,6 +28,7 @@ export default function TextForm(props) {
        text.select();
        
       document.execCommand("copy");
+      document.getSelection().removeAllRanges();
       }
       
     const handelExtraSpace=()=>{
@@ -37,28 +38,28 @@ export default function TextForm(props) {
       document.execCommand("copy");
       }
       
-  const [text,setText] = useState("Enter text here");
+  const [text,setText] = useState("");
 
  // setText("Sanjay");
   return (
     <>
     <div className='container'style={{color: props.mode=== 'dark'?'white':'black'}}>
-      <h1>{props.heading}</h1>
+      <h1 className='mb-2'>{props.heading}</h1>
     <div className="mb-3">
  
-  <textarea className = "form-control" id="myBox" value={text} onChange={handelOnChange}style={{backgroundColor:props.mode=== 'dark'?'gray':'white',color: props.mode=== 'dark'?'white':'black'}} rows="8"></textarea>
+  <textarea className = "form-control" id="myBox" value={text} onChange={handelOnChange}style={{backgroundColor:props.mode=== 'dark'?'#13466e':'white',color: props.mode=== 'dark'?'white':'black'}} rows="8"></textarea>
 </div>
-<button className="btn btn-primary mx-1 my-1"onClick={handelUpperCase}>Convert to UpperCase</button>
-<button className="btn btn-primary mx-1 my-1"onClick={handelLowerCase}>Convert to LowerCase</button>
-<button className="btn btn-primary mx-1 my-1"onClick={clear}>All Clear</button>
-<button className="btn btn-primary mx-1 my-1"onClick={handelCopy}>Copy Text</button>
-<button className="btn btn-primary mx-1 my-1"onClick={handelExtraSpace}>Handel Space</button>
+<button disabled ={text.length===0}className="btn btn-primary mx-1 my-1"onClick={handelUpperCase}>Convert to UpperCase</button>
+<button disabled ={text.length===0}className="btn btn-primary mx-1 my-1"onClick={handelLowerCase}>Convert to LowerCase</button>
+<button disabled ={text.length===0}className="btn btn-primary mx-1 my-1"onClick={clear}>All Clear</button>
+<button disabled ={text.length===0}className="btn btn-primary mx-1 my-1"onClick={handelCopy}>Copy Text</button>
+<button disabled ={text.length===0}className="btn btn-primary mx-1 my-1"onClick={handelExtraSpace}>Handel Space</button>
 
 </div>
 <div className='container my-3'style={{color: props.mode=== 'dark'?'white':'black'}}>
   <h2>Your text Summery</h2>
-  <p>{text.split(" ").length} word and {text.length} characters </p>
-  <p>{0.008 * text.split(" ").length} Minuets read </p>
+  <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} word and {text.length} characters </p>
+  <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minuets read </p>
 </div>
 </>
   )
